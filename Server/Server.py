d0371a479737ce39101f4b8492ea53388a6101bf
Hüseyin Sarsılmaz <huseyinsarsilmaz@hotmail.com>
2022-12-24 19:10:36 +0300

    Add connection to database

--- Server/Server.py
+++ Server/Server.py
@@ -1,7 +1,20 @@
 import socket
+import mysql.connector
+
+
 
 port = 3389
 sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+# connect to database with host localhost, user root, password 123456789, database gtutag3d
+db = mysql.connector.connect(
+    host="localhost", user="root", passwd="12345678", database="gtutag3d")
+if (db.is_connected()):
+    print("Connected to database")
+else:
+    print("Failed to connected to database")
+    exit(1)
+
+
 sock.bind(("", port))
 sock.listen()
 conn, addr = sock.accept()
