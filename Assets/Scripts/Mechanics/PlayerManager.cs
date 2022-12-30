8b3f4f30ed40a71f9f589b1845bd83ad58c2783d
Hüseyin Sarsılmaz <huseyinsarsilmaz@hotmail.com>
2022-12-30 23:37:58 +0300

    Add movement control

--- Assets/Scripts/Mechanics/PlayerManager.cs
+++ Assets/Scripts/Mechanics/PlayerManager.cs
@@ -1,12 +1,14 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System.Threading;
 
 public class PlayerManager : MonoBehaviour
 {
     private GameData gameData;
     private Networking networking;
     private Dictionary<int, GameObject> players;
+    public GameObject camera;
     private int myId;
 
     private string request;
@@ -19,6 +21,8 @@ public class PlayerManager : MonoBehaviour
         networking = FindObjectOfType<Networking>();
         players = new Dictionary<int, GameObject>();
         getPlayers();
+        //create a thread for updating player positions
+        Thread t = new Thread(new ThreadStart(updatePlayerPos));
 
     }
 
@@ -36,34 +40,54 @@ public class PlayerManager : MonoBehaviour
             players[int.Parse(player[0])].transform.position = new Vector3(float.Parse(player[2]), float.Parse(player[3]), float.Parse(player[4]));
         }
         myId = int.Parse(playerlist[2]);
+        camera.transform.position = new Vector3(players[myId].transform.position.x, players[myId].transform.position.y + 18, players[myId].transform.position.z - 20);
     }
 
     // fixed update is called once per frame
     void FixedUpdate()
     {
-        //detect w a s d and move player
+        //if w or s is pressed move forward or backward
         if (Input.GetKey(KeyCode.W))
         {
-            players[myId].transform.position += new Vector3(0, 0, 0.1f);
-        }
-        if (Input.GetKey(KeyCode.A))
-        {
-            players[myId].transform.position += new Vector3(-0.1f, 0, 0);
+            players[myId].transform.position += players[myId].transform.forward * 0.3f;
+            //move camera forward
+            camera.transform.position += players[myId].transform.forward * 0.3f;
         }
         if (Input.GetKey(KeyCode.S))
         {
-            players[myId].transform.position += new Vector3(0, 0, -0.1f);
+            players[myId].transform.position -= players[myId].transform.forward * 0.3f;
+            //move camera backward
+            camera.transform.position -= players[myId].transform.forward * 0.3f;
+        }
+        //if a or d is pressed rotate left or right
+        if (Input.GetKey(KeyCode.A))
+        {
+            players[myId].transform.Rotate(0, -1, 0);
+            //rotate camera around player
+            camera.transform.RotateAround(players[myId].transform.position, Vector3.up, -1);
         }
         if (Input.GetKey(KeyCode.D))
         {
-            players[myId].transform.position += new Vector3(0.1f, 0, 0);
+            players[myId].transform.Rotate(0, 1, 0);
+            //rotate camera around player
+            camera.transform.RotateAround(players[myId].transform.position, Vector3.up, 1);
         }
-        request = players[myId].transform.position.x + " " + players[myId].transform.position.y + " " + players[myId].transform.position.z;
-        positions = networking.updateMyPos(request).Split(' ');
-        for (int i = 0; i < positions.Length; i++)
+    }
+
+    void updatePlayerPos()
+    {
+        while (true)
         {
-            string[] player = positions[i].Split('-');
-            players[int.Parse(player[0])].transform.position = new Vector3(float.Parse(player[1]), float.Parse(player[2]), float.Parse(player[3]));
+            request = players[myId].transform.position.x + " " + players[myId].transform.position.y + " " + players[myId].transform.position.z + " " + players[myId].transform.rotation.x + " " + players[myId].transform.rotation.y + " " + players[myId].transform.rotation.z;
+            positions = networking.updateMyPos(request).Split(' ');
+            for (int i = 0; i < positions.Length; i++)
+            {
+                string[] player = positions[i].Split('-');
+                players[int.Parse(player[0])].transform.position = new Vector3(float.Parse(player[1]), float.Parse(player[2]), float.Parse(player[3]));
+            }
         }
     }
 }
+
+
+
